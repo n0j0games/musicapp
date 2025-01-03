92c3b4fe4da0ad5ca5e04c3973f6c3bfb4d2c428
Noah <66693138+n0j0games@users.noreply.github.com>
2025-01-03 19:28:25 +0100

    add 2025

--- src/app/common/resolver/aoty-aggregate.resolver.service.ts
+++ src/app/common/resolver/aoty-aggregate.resolver.service.ts
@@ -21,7 +21,7 @@ export class AotyAggregateResolverService implements Resolve<(AotyItem|HttpError
         }
         let aotyList = this.aotyService.getAotyList();
         const queryYears = aotyList?.items === undefined ?
-            [1960, 1970, 1980, 1990, 2000, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024] :
+            [1960, 1970, 1980, 1990, 2000, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025] :
             aotyList!.items!.map(value => value.year);
         const aotyItem = this.aotyService.getAggregatedAlbums(queryYears);
         if (aotyItem !== null) {
