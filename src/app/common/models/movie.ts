bd9ae3d1150d96c21e36538beb44dfaaada55d7a
Noah <66693138+n0j0games@users.noreply.github.com>
2025-01-26 12:12:04 +0100

    add 2025-04

--- src/app/common/models/movie.ts
+++ src/app/common/models/movie.ts
@@ -1,13 +1,12 @@
 export class Movie {
 
     constructor(public title : string,
-                public creator : string,
                 public year : number,
                 public rating : number | number[],
                 public url : string,
                 public imgSrc : string,
-                public seasons? : number,
-                public franchise? : string,
-                public activeSeason? : number) {}
+                public creator? : string,
+                public subtitle? : string
+    ) {}
 
 }
