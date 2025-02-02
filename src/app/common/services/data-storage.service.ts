4385b8637c5e5c99090f974e2087984acb3eeae0
Noah <66693138+n0j0games@users.noreply.github.com>
2025-02-02 18:58:28 +0100

    add series

--- src/app/common/services/data-storage.service.ts
+++ src/app/common/services/data-storage.service.ts
@@ -143,7 +143,7 @@ export class DataStorageService {
     }
 
     fetchSeriesItems(): Observable<(MotyItem | HttpErrorResponse)[]> {
-        const rawUrls = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+        const rawUrls = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
         const urls = rawUrls.map(url => this.fetchSingleSeriesItem(url));
         return forkJoin<(MotyItem | HttpErrorResponse)[]>(urls).pipe(
             map((value: (MotyItem | HttpErrorResponse)[]) => {
