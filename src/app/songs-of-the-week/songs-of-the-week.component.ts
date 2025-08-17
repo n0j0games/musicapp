db6d6903220fbc26192f7b432e775345cf8ac8b9
Noah <66693138+n0j0games@users.noreply.github.com>
2025-08-17 15:17:15 +0200

    add 2025-33

--- src/app/songs-of-the-week/songs-of-the-week.component.ts
+++ src/app/songs-of-the-week/songs-of-the-week.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {SotwService} from "../common/services/sotw.service";
 import {SongDetailComponent} from "./song-detail/song-detail.component";
 import {NgForOf, NgIf} from "@angular/common";
@@ -13,7 +13,8 @@ import {Logger} from "../common/logger";
   imports: [
     SongDetailComponent,
     NgForOf,
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './songs-of-the-week.component.html'
 })
@@ -26,11 +27,22 @@ export class SongsOfTheWeekComponent implements OnInit {
 
   fridayOfTheWeek! : string;
 
+  sotwList!: { year : number, week : number, preview? : string[] }[];
+
+  previousWeekLink: string | undefined;
+  nextWeekLink: string | undefined;
+
   private logger: Logger = new Logger(this);
 
   constructor(private route: ActivatedRoute, private router: Router, private sotwService : SotwService) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.loadData();
+    });
+  }
+
+  private loadData() {
     const week = this.route.snapshot.paramMap.get('week');
     if (week === undefined || week === null) {
       this.router.navigate(['**']).then(() => this.logger.error("Undefined week, routed to 404"));
@@ -46,6 +58,48 @@ export class SongsOfTheWeekComponent implements OnInit {
     }
     this.songsOfTheWeek.songs = this.songsOfTheWeek.songs.sort((a, b) => b.rating - a.rating);
     this.fridayOfTheWeek = WeekHelper.getFridayOfWeek(this.songsOfTheWeek?.week, this.songsOfTheWeek?.year )
+
+    const sotwList = this.sotwService.getSotwList();
+    if (sotwList == null) {
+      this.router.navigate(['**']).then(() => this.logger.error("Empty list, routed to 404"));
+      return;
+    }
+    this.sotwList = [...sotwList.items!].sort((a, b) => a.year - b.year || a.week - b.week).filter(value => value.week !== <number><unknown>'00');
+
+    this.getPreviousWeek();
+    this.getNextWeek();
+  }
+
+  private getNextWeek() {
+    const currentItem = this.getCurrentWeeksIndex();
+    console.log('cur', this.sotwList, currentItem);
+    if (currentItem === - 1 || currentItem >= this.sotwList.length - 1) {
+      this.nextWeekLink = undefined;
+      return;
+    }
+    const nextItem = this.sotwList[currentItem+1];
+    console.log('ni', nextItem);
+    this.nextWeekLink = nextItem.year.toString() + nextItem.week.toString();
+  }
+
+  private getPreviousWeek() {
+    const currentItem = this.getCurrentWeeksIndex();
+    console.log('cur', currentItem);
+    if (currentItem <= 0) {
+      this.previousWeekLink = undefined;
+      return;
+    }
+    const prevItem = this.sotwList[currentItem-1];
+    console.log('pi', prevItem);
+    this.previousWeekLink = prevItem.year.toString() + prevItem.week.toString();
+  }
+
+  private getCurrentWeeksIndex(): number {
+    const item = this.sotwList.filter(value => value.year === this.activeYear && value.week === this.activeWeek);
+    if (item.length !== 1) {
+      return -1;
+    }
+    return this.sotwList.indexOf(item[0]);
   }
 
 }
